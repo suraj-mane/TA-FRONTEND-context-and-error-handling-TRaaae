164b8279e1991b1f4d8a6fff577e4b67c8c32bd5
suraj-mane <sun.mane1100@gmail.com>
2022-09-19 15:50:51 +0530

    add HOC

---
+++ block-BRaadb/react-hoc/src/Articles.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Link, withRouter } from "react-router-dom";
+import articles from "./data/articles.json";
+
+class Articles extends React.Component {
+  state = {
+    searchTerm: ""
+  };
+  handleChange = ({ target: { value } }) => {
+    this.setState({ searchTerm: value });
+  };
+  render() {
+    let filteredArticles = articles.filter((article) =>
+      article.title.toLowerCase().includes(this.state.searchTerm)
+    );
+    return (
+      <>
+        <input
+          placeholder="Search"
+          className="search"
+          value={this.state.searchTerm}
+          onChange={this.handleChange}
+        />
+        <ul className="articles">
+          {filteredArticles.map((article) => (
+            <li>
+              <Link to={"articles/" + article.slug}>
+                <h3>{article.title}</h3>
+              </Link>
+              <small>{article.author}</small>
+            </li>
+          ))}
+        </ul>
+      </>
+    );
+  }
+}
+
+export default withRouter(Articles);
